c51e51fed06e42dea389a000f595110a818d71a5
anny21 <34449508+anny21@users.noreply.github.com>
2019-09-19 18:37:47 +0100

    Hannah Edited this files

---
+++ layout/header.php
@@ -0,0 +1,19 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- fontawesome cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Styles link -->
+    <title>Login page</title>
+    <link href="https://fonts.googleapis.com/css?family=Merienda|Montserrat|Oleo+Script&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+    <!-- all code go here -->
+ 
+  
